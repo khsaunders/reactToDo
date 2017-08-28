fb393f1ab9c38ef8c6e1d0c829f18a29b63fb901
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-08-28 12:33:07 -0400

    Eliminate unnecessary code

--- src/CreateToDo.js
+++ src/CreateToDo.js
@@ -2,29 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.css';
 
-// export default class CreateToDo extends Component {
-//   constructor(props){
-//     super(props);
-//
-//     this.state= {value: ''}
-//
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <form>
-//           <input type="text" placeholder="what do you need to do?">
-//           </input>
-//           </form>
-//         <button>add</button>
-//       </div>
-//     )
-//   }
-// }
-
 export default class CreateToDo extends React.Component {
   constructor(props) {
     super(props);
